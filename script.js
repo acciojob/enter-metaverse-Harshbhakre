b587e790421563bf6cf52c21351bab25ac53df76
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-18 15:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector("#enterBtn").addEventListener("click",()=>{
   let ptag = document.querySelector("#status")
   let htag = document.createElement("h1")
-	htag.innerHTML = ptag.innerHTML
+	htag.innerHTML = "Entered Metaverse"
 	htag.id = ptag.id
 	ptag.parentNode.replaceChild(htag,ptag)
 })
