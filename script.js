259262fd1db0578fb5b4e84e03ef1b2244b7a289
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-18 15:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.querySelector("#enterBtn").addEventListener("click",()=>{
+  let ptag = document.querySelector("#status")
+  let htag = document.createElement("h1")
+	htag.innerHTML = ptag.innerHTML
+	htag.id = ptag.id
+	ptag.parentNode.replaceChild(htag,ptag)
+})
